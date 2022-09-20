0a155b6b4f3976534e9a5d3a69104ef6e8d0ef3e
lennertvandevelde <lennert.van.de.velde@outlook.be>
2022-09-20 14:55:54 +0200

    Changes to infra block

--- infra_block.py
+++ infra_block.py
@@ -1,4 +1,3 @@
-from ast import arg
 from prefect.infrastructure.docker import DockerContainer
 import argparse
 
@@ -12,6 +11,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Save a Docker container")
     parser.add_argument("--image", required=True, help="Docker image to run")
     parser.add_argument("--name", required=True, help="Name of the block")
-    parser.add_argument("--registery", required=False, help="Docker registery (the block which it contains)")
+    parser.add_argument("--registery", required=False, help="Docker registery (the block which it contains), default docker.io")
     args = parser.parse_args()
     save_image(args.image, args.name, args.registery)
